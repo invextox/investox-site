8fcbe52e742fcfcd8b8a2dfec30cce6e4c86eaa9
invextox <investixpro@gmail.com>
2025-04-19 19:58:12 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
+import Hero from '../components/Hero';
+
 export default function Home() {
   return (
-    <div style={{ fontFamily: 'Arial', textAlign: 'center', marginTop: '50px' }}>
-      <h1>Welcome to Investox.io</h1>
-      <p>The future of DeFi investing. Connect your wallet to get started.</p>
-      <button style={{ padding: '10px 20px', marginTop: '20px' }}>Connect MetaMask</button>
-    </div>
+    <>
+      <Hero />
+    </>
   );
 }
