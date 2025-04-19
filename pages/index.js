78361b407d930596c96d7b8839b43ece0b9e6375
invextox <investixpro@gmail.com>
2025-04-19 23:22:05 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Hero from '../components/hero';
 import WhyInvestox from '../components/whyInvestox';
 import Features from '../components/Features';
 import CallToAction from '../components/CallToAction';
+import Footer from '../components/Footer';
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <WhyInvestox />
       <Features />
       <CallToAction />
+      <Footer />
     </>
   );
 }
