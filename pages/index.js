33f86cd4f82c88a32cff3c5310bc043cafd0d874
invextox <investixpro@gmail.com>
2025-04-19 22:49:14 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import Hero from '../components/hero';
 import WhyInvestox from '../components/whyInvestox';
+import Features from '../components/Features';
 
 export default function Home() {
   return (
     <>
       <Hero />
       <WhyInvestox />
+      <Features /> {/* ← זה מה שחסר אצלך */}
     </>
   );
 }
