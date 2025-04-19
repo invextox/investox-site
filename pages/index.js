0c52e419dd9b61552c455a68ecc67e8e084dc889
invextox <investixpro@gmail.com>
2025-04-19 20:29:08 +0300

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Hero from '../components/hero';
+import WhyInvestox from '../components/WhyInvestox';
+import HowItWorks from '../components/HowItWorks';
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <WhyInvestox />
+      <HowItWorks />
+    </>
+  );
+}
